e6df8729ccc68114da70d37600c183838221f7da
Kazuya Murakami <kazuya.murakami@jp.bosch.com>
2023-12-29 05:05:39 +0000

    initial commit

---
+++ capital_cities/capitals/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+
+
+class Capital(models.Model):
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+
+    def get_absolute_url(self):
+        return reverse("home")
